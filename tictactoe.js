cb18b2d1e78c71a75280cb3828b0205e94718e4c
DraconicOv <theredlionzzw@gmail.com>
2024-05-13 12:38:31 -0400

    Added play round function

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,15 @@
 function getComputerChoice(){
     return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
 }
-console.log(getComputerChoice());
+
+function playRound(humanChoice, computerChoice){
+    results = {
+        "rock": {"rock": "tie", "paper": "lose", "scissors": "win"},
+        "paper": {"rock": "win", "paper": "tie", "scissors": "lose"},
+        "scissors": {"rock": "lose", "paper": "win", "scissors": "tie"}
+    }
+    return results[humanChoice][computerChoice];
+}
+var humanScore, computerScore;
+computerScore = humanScore = 0;
+
