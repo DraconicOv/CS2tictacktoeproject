b6e71d92133424b35359b171998c8c82d17c82f2
DraconicOv <theredlionzzw@gmail.com>
2024-05-13 12:44:21 -0400

    added playGame function()

--- tictactoe.js
+++ tictactoe.js
@@ -3,16 +3,23 @@ function getComputerChoice(){
 }
 var humanScore, computerScore;
 computerScore = humanScore = 0;
-
-function playRound(humanChoice, computerChoice){
-    results = {
-        "rock": {"rock": "tie", "paper": "lose", "scissors": "win"},
-        "paper": {"rock": "win", "paper": "tie", "scissors": "lose"},
-        "scissors": {"rock": "lose", "paper": "win", "scissors": "tie"}
+function playGame(){
+    function playRound(humanChoice, computerChoice){
+        results = {
+            "rock": {"rock": "tie", "paper": "lose", "scissors": "win"},
+            "paper": {"rock": "win", "paper": "tie", "scissors": "lose"},
+            "scissors": {"rock": "lose", "paper": "win", "scissors": "tie"}
+        }
+        result = results[humanChoice.toLowerCase()][computerChoice.toLowerCase()];
+        result == "win" ? humanScore++ : result == "lose" ? computerScore++ : null;
+        return result;
     }
-    result = results[humanChoice.toLowerCase()][computerChoice.toLowerCase()];
-    result == "win" ? humanScore++ : result == "lose" ? computerScore++ : null;
-    return result;
+    for(var i = 0; i<5; i++){
+        humanChoice = prompt("Rock, paper, or scissors?");
+        computerChoice = getComputerChoice();
+        console.log("You chose " + humanChoice + " and the computer chose " + computerChoice);
+        console.log("You " + playRound(humanChoice, computerChoice) + "!");
+    }
+    console.log("The final score is " + humanScore + " to " + computerScore + ".")
 }
-
-
+playGame()
